9e13eb0c44cbac02ecabc1039d63ea8160f229da
Techy_Kartik <41766740+anshfuke@users.noreply.github.com>
2020-01-19 10:48:00 +0530

    Create banner4.py

---
+++ banner/banner4.py
@@ -0,0 +1,10 @@
+# Set Color
+L = '\033[1;36m' #L
+print ""+L+""
+print " ____________"
+print "< UltraHX >"
+print " ------------"
+print "       \   ,__,"
+print "        \  (oo)____"
+print "           (__)    )\ "
+print "              ||--|| *"
